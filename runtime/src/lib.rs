a149c1f72a4c62577b40811b67437dbcee392ee2
David Rhodus <david@vacovideo.com>
2022-03-17 20:55:53 -0700

    Add in code for name-service.
    We are pulling in nicks pallet from https://github.com/paritytech/substrate/blob/master/frame/nicks/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -316,6 +316,41 @@ impl pallet_site::Config for Runtime {
     type SiteIndex = u32;
 }
 
+/// Add this code block to your template for Name Service / Nick Names:
+parameter_types! {
+  // Choose a fee that incentivizes desireable behavior.
+  pub const NickReservationFee: u128 = 1_000_000_000_000;
+  pub const MinNickLength: u32 = 8;
+  // Maximum bounds on storage are important to secure your chain.
+  pub const MaxNickLength: u32 = 32;
+}
+
+impl pallet_name_service::Config for Runtime {
+    // The Balances pallet implements the ReservableCurrency trait.
+    // `Balances` is defined in `construct_runtime!` macro. See below.
+    // https://docs.substrate.io/rustdocs/latest/pallet_balances/index.html#implementations-2
+    type Currency = Balances;
+
+    // Use the NickReservationFee from the parameter_types block.
+    type ReservationFee = NickReservationFee;
+
+    // No action is taken when deposits are forfeited.
+    type Slashed = ();
+
+    // Configure the FRAME System Root origin as the Nick pallet admin.
+    // https://docs.substrate.io/rustdocs/latest/frame_system/enum.RawOrigin.html#variant.Root
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+
+    // Use the MinNickLength from the parameter_types block.
+    type MinLength = MinNickLength;
+
+    // Use the MaxNickLength from the parameter_types block.
+    type MaxLength = MaxNickLength;
+
+    // The ubiquitous event type.
+    type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
     pub enum Runtime where
@@ -340,6 +375,7 @@ construct_runtime!(
         Note: pallet_note::{Pallet, Storage, Call, Event<T>},
         // YibanChen Substrate Site pallet
         Site: pallet_site::{Pallet, Storage, Call, Event<T>},
+        NameService: pallet_name_service::{Pallet, Call, Storage, Event<T>},
     }
 );
 
